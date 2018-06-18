3fa934a849edd80fa1f50c3a5a9b4e2789b2c86f
Shane Thacker <shane@steadymade.com>
2018-06-17 21:14:33 -0500

    cleanup

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const getCallsite = (layer) => layer
   .replace(process.cwd(), '');
 
 module.exports = (opts) => {
-  console.log('opts', opts);
   const engine = require(opts.name);
   const engineInstance = engine(opts.opts);
   let render;
@@ -16,15 +15,6 @@ module.exports = (opts) => {
 
   return (template, data, callback) => {
     const path = `${getCallsite(callsites()[2])}/${template}`;
-    // console.log('\n');
-    // console.log('\n');
-    // console.log('path 1', `${getCallsite(callsites()[1])}/${template}`);
-    // console.log('path 2', `${getCallsite(callsites()[2])}/${template}`);
-    // console.log('path 3', `${getCallsite(callsites()[3])}/${template}`);
-    // console.log('\n');
-    // console.log('\n');
-    // console.log('path', path);
-    // console.log('template', template);
     return render(path, data);
   }
 }
